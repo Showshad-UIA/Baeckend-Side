af9f6bda114218adf6be1c95da790c18597876ed
raihan <raihanulmcse@mail.com>
2022-05-19 00:10:15 +0800

    code modified

--- index.js
+++ index.js
@@ -67,13 +67,14 @@ async function run() {
 			const result = await productCollection.insertOne(doc);
 			res.send(result);
 		});
-		//DELETE
+		//delete
 		app.delete("/products/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: ObjectId(id) };
 			const result = await productCollection.deleteOne(query);
 			res.send(result);
 		});
+		// will use later
 	} finally {
 		// await client.close();
 	}
